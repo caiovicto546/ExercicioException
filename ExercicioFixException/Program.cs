66870a9514778e38e4d1964c27a767689c1cd324
caiovicto546 <caiovicto@gmail.com>
2019-01-10 12:53:12 -0200

    Adicionar arquivos de projeto.

---
+++ ExercicioFixException/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioFixException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
